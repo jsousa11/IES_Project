d0f9196169e6e646db6731f259759365d94b9763
Vania 102383 <vania.morais@ua.pt>
2022-11-10 17:54:05 +0000

    Static website

---
+++ easyfarming/src/main/java/com/ProjetoIES/easyfarming/EasyfarmingApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetoIES.easyfarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyfarmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyfarmingApplication.class, args);
+	}
+
+}
---
+++ easyfarming/src/main/java/com/ProjetoIES/easyfarming/controller/Controller1.java
@@ -0,0 +1,19 @@
+package com.ProjetoIES.easyfarming.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class Controller1 {
+    @GetMapping("/about.html")
+    public String about() {
+        return "about.html";
+    }
+}
