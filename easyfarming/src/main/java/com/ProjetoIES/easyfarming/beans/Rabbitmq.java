311bd68400770f923bde8cd89e4c5e5a08b50ec5
marcquesa <77558488+marcquesa@users.noreply.github.com>
2022-12-13 11:36:11 +0000

    Rabbitmq @Service

---
+++ easyfarming/src/main/java/com/ProjetoIES/easyfarming/beans/Rabbitmq.java
@@ -0,0 +1,26 @@
+package com.ProjetoIES.easyfarming.beans;
+
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Rabbitmq {
+    @RabbitListener(queues = "generator")
+    public void receiveMessage(String jsonMessage) {
+        // convert to object
+        JsonMapper mapper = new JsonMapper();
+
+        try {
+            Message message = mapper.readValue(jsonMessage, Message.class);
+            if (message.getTemperature() > 25) {
+                System.out.println("ALERT: Temperature is high!");
+            }
+            if (message.getHumidity() < 50) {
+                System.out.println("ALERT: Humidity is low!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
