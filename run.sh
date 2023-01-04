7af7d16b2412468387d994e60ecbe8350e570d0d
11Souz11 <jsousa11@ua.pt>
2023-01-04 15:24:40 +0000

    docker and run instructions

---
+++ run.sh
@@ -0,0 +1,7 @@
+
+docker run --name easyfarming -e MYSQL_ROOT_PASSWORD=secret1 -e MYSQL_DATABASE=easyfarming -e MYSQL_USER=easyfarming -e MYSQL_PASSWORD=secret2 -p 33060:3306 -d mysql/mysql-server:5.7
+cd generator/
+python3 main.py
+cd ../easyfarming/
+mvn package
+mvn spring-boot:run
